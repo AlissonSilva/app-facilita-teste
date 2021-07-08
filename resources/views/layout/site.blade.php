92fd6f8f38758c9f27b009b3d9f49b0b22684758
AlissonSilva <alisson_sao@hotmail.com>
2021-07-08 07:43:37 -0300

    Commit 08/07/2021
    Definição do layout (blade)

---
+++ resources/views/layout/site.blade.php
@@ -0,0 +1,10 @@
+
+@include('layout._includes.topo')
+
+<div id="content-wrapper" class="d-flex flex-column">
+    <div id="content">
+        @yield('conteudo')
+    </div>
+</div>
+@include('layout._includes.rodape')
+
---
+++ resources/views/problema_1/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.site')
+
+@section('titulo','AppFacilita: Problema 01')
+
+@section('conteudo')
+    teste
+@endsection
